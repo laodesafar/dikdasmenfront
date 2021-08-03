baeb1f0ef99bbd922e0cefc11398d853d6b5f17b
La Ode Safar <laodesafar94@gmail.com>
2021-08-03 17:09:07 +0700

    initialize

---
+++ beranda/hero-video.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export default function HeroVideo() {
+  return (
+    <Fragment>
+      <section className="h-screen mx-auto">
+        <h1>Disini Section Hero Video</h1>
+      </section>
+    </Fragment>
+  );
+}
