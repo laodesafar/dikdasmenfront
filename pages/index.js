7aaab07b2a369f74a6602fb5a97ea7e8aa29001d
La Ode Safar <laodesafar94@gmail.com>
2021-08-06 23:35:22 +0700

    tambah alias

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
-import Navbar from "../components/Layout/Navbar";
-import HeroVideo from "../components/Layout/HeroVideo";
+import Navbar from "@components/Layout/Navbar";
+import HeroVideo from "@components/Layout/HeroVideo";
 
 export default function Home() {
   return (
