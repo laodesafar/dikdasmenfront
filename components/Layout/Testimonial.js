f26c07a0b9659f3d06c6249671b1cb8aba5b127d
La Ode Safar <laodesafar94@gmail.com>
2021-08-12 15:33:37 +0700

    tambahkan agenda

---
+++ components/Layout/Testimonial.js
@@ -0,0 +1,13 @@
+import ButtonLengkap from "@components/UI/ButtonLengkap";
+import { VscQuote } from "react-icons/vsc";
+import CardTesti from "@components/UI/CardTesti";
+
+export default function Testimonial() {
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-2xl capitalize font-semibold my-4 md:mt-10 md:mb-5 lg:text-3xl text-green-500 ">
+        Kata Mereka Tentang Dikdasmen
+      </h2>
+    </div>
+  );
+}
