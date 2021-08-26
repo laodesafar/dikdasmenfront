989eebb83d822a32c22b1636f8aff4b1c956055c
La Ode Safar <laodesafar94@gmail.com>
2021-08-26 13:38:24 +0700

    responsive navbar

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -4,11 +4,11 @@ import { Menu, Transition, Dialog } from "@headlessui/react";
 import Link from "next/link";
 
 import Logo from "../UI/Logo";
-import Modal from "../UI/Modal";
-import { BiSearch, BiChevronDown, BiX } from "react-icons/bi";
+import { BiSearch, BiChevronDown, BiX, BiMenu } from "react-icons/bi";
 
 export default function Navbar() {
   const [showModal, setShowModal] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const tentangList = [
     { text: "Sejarah", href: "/tentang/sejarah" },
@@ -35,30 +35,46 @@ export default function Navbar() {
                 </a>
               </Link>
             </div>
-            <div className="ml-auto">
-              <ul className="flex space-x-10 items-center  ">
-                <Menu as="li" className="relative ">
-                  <Menu.Button className="hover:text-green-500 cursor-pointer py-6 flex items-center transition-colors duration-300 ease-in-out outline-none">
+            <div
+              className={`ml-auto fixed inset-0 transition-all duration-100 ${
+                showMenu ? "bg-black/50 visible" : "invisible lg:visible"
+              }   lg:relative lg:block `}
+            >
+              <ul
+                className={`w-1/3 lg:w-auto fixed lg:absolute bg-gray-100 lg:bg-transparent transition-all duration-300 ease-in-out ${
+                  showMenu ? "right-0" : "-right-full"
+                } top-0    bottom-0 lg:flex lg:space-x-10 lg:items-center pb-0 lg:px-0 px-10 pt-20 lg:mt-auto`}
+              >
+                <div onClick={() => setShowMenu(!showMenu)}>
+                  <BiX className="h-10 w-10 absolute lg:hidden  right-5 top-5 z-100 cursor-pointer hover:text-red-500 transition-colors duration-300 ease-in-out " />
+                </div>
+                <Menu
+                  as="li"
+                  className="relative pb-3 mb-3 border-b-2 border-green-500 lg:border-none lg:pb-0 lg:mb-0"
+                >
+                  <Menu.Button className="hover:text-green-500 cursor-pointer lg:py-6 flex items-center transition-colors duration-300 ease-in-out outline-none">
                     Tentang <BiChevronDown />
                   </Menu.Button>
                   <Transition
                     as={Fragment}
                     enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
+                    enterFrom="transform opacity-0  scale-95"
+                    enterTo="transform opacity-100  scale-100"
                     leave="transition ease-in duration-75"
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute w-max min-w-[200px] outline-none border-green-500 border-t-4 ">
-                      {tentangList.map(({ text, href }) => (
-                        <Menu.Item key={text}>
+                    <Menu.Items className="block lg:absolute mt-2 lg:mt-0 w-full lg:w-max min-w-[200px] outline-none border-green-500 border-t-2 lg:border-t-4 ">
+                      {tentangList.map(({ text, href }, i) => (
+                        <Menu.Item className="last:border-b-0" key={text}>
                           {({ active }) => (
                             <Link href={href}>
                               <a
                                 className={` ${
                                   active && "text-green-500"
-                                } flex py-3 px-6 bg-gray-50
+                                } flex py-3 px-6 bg-white lg:border-none
+                                    border-b-[1px] border-green-500
+                                lg:bg-gray-50
                         hover:text-green-500 transition-colors duration-300 ease-in-out `}
                               >
                                 {text}
@@ -71,7 +87,7 @@ export default function Navbar() {
                   </Transition>
                 </Menu>
                 {/* </li> */}
-                <li>
+                <li className="pb-3 mb-3 border-b-2 border-green-500 lg:border-none lg:pb-0 lg:mb-0">
                   <Link href="/pendidikan">
                     <a className="hover:text-green-500 py-6 transition-colors duration-300 ease-in-out">
                       Pendidikan
@@ -79,8 +95,11 @@ export default function Navbar() {
                   </Link>
                 </li>
 
-                <Menu as="li" className="relative ">
-                  <Menu.Button className="hover:text-green-500 cursor-pointer py-6 flex items-center transition-colors duration-300 ease-in-out outline-none">
+                <Menu
+                  as="li"
+                  className="relative pb-3 mb-3 border-b-2 border-green-500 lg:border-none lg:pb-0 lg:mb-0"
+                >
+                  <Menu.Button className="hover:text-green-500 cursor-pointer lg:py-6 flex items-center transition-colors duration-300 ease-in-out outline-none">
                     Galeri <BiChevronDown />
                   </Menu.Button>
                   <Transition
@@ -92,15 +111,16 @@ export default function Navbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute w-max min-w-[200px] outline-none border-green-500 border-t-4 ">
+                    <Menu.Items className="block lg:absolute w-full mt-2 lg:mt-0 lg:w-max min-w-[200px] outline-none border-green-500 border-t-2 lg:border-t-4 ">
                       {galeriList.map(({ text, href }) => (
                         <Menu.Item key={text}>
                           {({ active }) => (
                             <Link href={href}>
                               <a
-                                className={` ${
+                                className={`  ${
                                   active && "text-green-500"
-                                } flex py-3 px-6 bg-gray-50
+                                } flex py-3 px-6 bg-white lg:border-none
+                                border-b-[1px] border-green-500
                         hover:text-green-500 transition-colors duration-300 ease-in-out `}
                               >
                                 {text}
@@ -112,7 +132,7 @@ export default function Navbar() {
                     </Menu.Items>
                   </Transition>
                 </Menu>
-                <li>
+                <li className="pb-3 mb-3 border-b-2 border-green-500 lg:border-none lg:pb-0 lg:mb-0">
                   <Link href="/kontak">
                     <a className="hover:text-green-500 py-6 transition-colors duration-300 ease-in-out">
                       Kontak
@@ -121,7 +141,7 @@ export default function Navbar() {
                 </li>
               </ul>
             </div>
-            <div className="ml-10 cursor-pointer hover:text-green-500 transition-colors duration-300 ease-in-out">
+            <div className="ml-auto lg:ml-10 cursor-pointer hover:text-green-500 transition-colors duration-300 ease-in-out">
               <a onClick={() => setShowModal(!showModal)}>
                 <BiSearch className="text-xl" />
               </a>
@@ -166,6 +186,15 @@ export default function Navbar() {
                   </div>
                 </Dialog>
               </Transition>
+            </div>
+            <div className="mx-10 block lg:hidden cursor-pointer hover:text-green-500 transition-colors duration-300 ease-in-out">
+              <a onClick={() => setShowMenu(!showMenu)}>
+                {showMenu ? (
+                  <BiX className="text-2xl transition-all" />
+                ) : (
+                  <BiMenu className="text-2xl" />
+                )}
+              </a>
             </div>
           </div>
         </div>
